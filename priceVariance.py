ebde4f9014dd623bd4067718312d2d9e080a48e0
Brian <bmccormack15@gmail.com>
2021-12-09 04:35:03 -0800

    added encoding for #6 but the values seem to high, need to check what the standard Quantity is

--- priceVariance.py
+++ priceVariance.py
@@ -4,7 +4,8 @@ def calcPriceVar(item):
     
 def calcQuantityVar(item):
 	return item['standardrate'] * (item['actualQ'] - item['standardQ'])
-    
+
+#5    
 numKayaks = 35
 items = {
 'powder': {'actualrate': 3980 / 1990, 'standardrate': 1.50, 'actualQ': 1990.0 / numKayaks, 'standardQ': 54},
@@ -12,9 +13,22 @@ items = {
 'type1worker': {'actualrate': 2020/101, 'standardrate': 20, 'actualQ': 101/numKayaks, 'standardQ': 3},
 'type2worker': {'actualrate': 2518.50/146, 'standardrate': 17, 'actualQ': 146/numKayaks, 'standardQ': 4}
 }
+
+#6 - double check encoding
+items2 = {
+'materials': {'actualrate': 122.00, 'standardrate': 124.00, 'actualQ': 1176, 'standardQ': 1140},
+'labour': {'actualrate': 16.00, 'standardrate': 13.00, 'actualQ': 4120, 'standardQ': 4340}
+}
+
 for item in items:
 	print("#", item, "#", sep="")
 	print('price', calcPriceVar(items[item]))
 	print('quantity', calcQuantityVar(items[item]))
 	print()
 
+for item in items2:
+	print("###q6###")
+	print("#", item, "#", sep="")
+	print('price', calcPriceVar(items2[item]))
+	print('quantity', calcQuantityVar(items2[item]))
+	print()
