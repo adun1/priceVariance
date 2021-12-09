2d6c09fc456e3b55dff5f90b0e254f9041d82a64
Brian <bmccormack15@gmail.com>
2021-12-09 04:07:19 -0800

    restructured data, fixed logic error

--- priceVariance.py
+++ priceVariance.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 
 def calcPriceVar(item):
-	return item['standardQ'] * (item['actualrate'] - item['standardrate'])
+	return item['actualQ'] * (item['actualrate'] - item['standardrate'])
     
 def calcQuantityVar(item):
 	return item['standardrate'] * (item['actualQ'] - item['standardQ'])
     
-type1worker = {'actualrate': 2020/101, 'standardrate': 20, 'actualQ': 101/35, 'standardQ': 3}
-print(calcPriceVar(type1worker))
-print(calcQuantityVar(type1worker))
+numKayaks = 35
+items = {
+'powder': {'actualrate': 3980 / 1990, 'standardrate': 1.50, 'actualQ': 1990.0 / numKayaks, 'standardQ': 54}, 
+'type1worker': {'actualrate': 2020/101, 'standardrate': 20, 'actualQ': 101/numKayaks, 'standardQ': 3}
+}
+for item in items:
+	print("#", item, "#", sep="")
+	print('price', calcPriceVar(items[item]))
+	print('quantity', calcQuantityVar(items[item]))
+
