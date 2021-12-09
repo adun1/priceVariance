e51b21530ac0c18a2c6e8324fc4166a8bf151e37
Brian <bmccormack15@gmail.com>
2021-12-09 03:43:52 -0800

    price and quantity var

--- priceVariance.py
+++ priceVariance.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 def calcPriceVar(item):
-    return item['standardQ'] * (item['actualrate'] - item['standardrate'])
+	return item['standardQ'] * (item['actualrate'] - item['standardrate'])
     
-def calcQuantityVar():
-    return
+def calcQuantityVar(item):
+	return item['standardrate'] * (item['actualQ'] - item['standardQ'])
     
-type1worker = {actualrate: 2020/101, standardrate: 20, actualQ: 101/35, standardQ: 3}
+type1worker = {'actualrate': 2020/101, 'standardrate': 20, 'actualQ': 101/35, 'standardQ': 3}
 print(calcPriceVar(type1worker))
+print(calcQuantityVar(type1worker))
